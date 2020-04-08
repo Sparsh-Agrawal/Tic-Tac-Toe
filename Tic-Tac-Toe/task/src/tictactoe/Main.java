39eb7599db379acd7969d246dd87c4954846b707
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-04-08 13:15:48 +0530

    stage 1 completed

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -2,6 +2,7 @@ package tictactoe;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here 
+        // write your code here
+        System.out.println("X O X");
     }
 }
