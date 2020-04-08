b86038263a4469bd9d1fc6dcb1315ccfebafcec6
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-04-08 14:31:39 +0530

    stage 2 completed

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,8 +1,24 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("X O X");
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        int c = 0;
+        System.out.println("---------");
+        for(int i=0;i<3;i++)
+        {
+            System.out.print("| ");
+            for(int j=0;j<3;j++)
+            {
+                System.out.print(s.charAt(c)+" ");
+                c++;
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+        System.out.println("---------");
     }
 }
