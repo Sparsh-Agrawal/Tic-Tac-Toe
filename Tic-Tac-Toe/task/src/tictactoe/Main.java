2212694de75e40943c1d7a73aa06745905ee16df
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-04-09 12:28:34 +0530

    stage 4 completed

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -168,6 +168,7 @@ public class Main {
 //            System.out.println(I5);
     }
 
+
     static void printFields(String s) {
         int c=0;
         System.out.println("---------");
@@ -182,6 +183,7 @@ public class Main {
         }
         System.out.println("---------");
     }
+
 
     static void check(String co) throws NotEmptyException {
         int c=0;
@@ -221,7 +223,84 @@ public class Main {
         } else {
             throw new NotEmptyException("This cell is occupied! Choose another one!");
         }
-
     }
 
+
+    static boolean checkWin(String s){
+        int[] arr = { 0,1,2, 3,4,5, 6,7,8, 0,3,6, 1,4,7, 2,5,8, 0,4,8, 2,4,6 };
+        String G1 = "Game not finished";
+        String D2 = "Draw";
+        String X3 = "X wins";
+        String O4 = "O wins";
+        String I5 = "Impossible";
+        int t = 0;
+        int y = 0;
+
+        //for win and impossible
+        if(t==0) {
+            for (int i = 0; i < arr.length / 3; i++) {
+                int p = i * 3;
+                if (s.charAt(arr[p]) == s.charAt(arr[p + 1]) && s.charAt(arr[p]) == s.charAt(arr[p + 2])) {
+                    if (s.charAt(arr[p]) == 'X') {
+                        t = 3;
+                       // y = 4;
+                        //break;
+                    } else {
+                       // t = 4;
+                        y = 3;
+                        //break;
+                    }
+                }
+            }
+        }
+
+        // for impossible
+        if(t==0 && y==0) {
+            for(int i=0;i<s.length();i++) {
+                if(s.charAt(i)=='X')
+                    t++;
+                else if(s.charAt(i)=='O')
+                    y++;
+            }
+            if(t-y>1 || y-t>1)
+            {
+                t=3;
+                y=3;
+            }
+            else
+            {
+                t=0;
+                y=0;
+            }
+        }
+
+        if(t==0 && y==0)
+        {
+            for(int i=0;i<s.length();i++) {
+                if (s.charAt(i) == ' ') {
+                    t = 1;
+                    break;
+                }
+            }
+            if(t!=1)
+                t=2;
+        }
+
+        if(t==1)
+            System.out.println(G1);
+        else if(t==2)
+            System.out.println(D2);
+        else if (t==3 && y!=3) {
+            System.out.println(X3);
+            return true;
+        }
+        else if (t!=3 && y==3) {
+            System.out.println(O4);
+            return true;
+        }
+        else if (t==3 && y==3)
+            System.out.println(I5);
+
+        return false;
+    }
 }
